0efbef1f54b2a36a2b5f4de742e5f89c68b85243
Ibtisam Hemmo <ibtisamhemmo@gmail.com>
2021-04-10 23:23:21 +0300

    commit

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import {Link} from 'react-router-dom';
 function About() {
     return (
         <div>
-            <h4> Version 1.0.1 </h4>
+            <h4> Page 1.0 </h4>
             <Link to="/">Go back </Link>
         </div>
     )
